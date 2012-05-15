27a427d56acca22d331305d059f67a3cfa97cf62
Tom Bombadil <amanibhavam@destructuring.org>
2012-05-14 23:54:08 -0700

    needed to add tarballs afterall

---
+++ sources/rsync.macports.org/release/tarballs/ports/_resources/port1.0/livecheck/fallback.tcl
@@ -0,0 +1,6 @@
+# $Id: fallback.tcl 90212 2012-02-26 17:25:01Z jmr@macports.org $
+#
+# If the default type cannot be determined from the mirror, this file is
+# sourced.
+
+source [getdefaultportresourcepath "port1.0/livecheck"]/freecode.tcl
